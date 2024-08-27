9a42e9b121056626e0e0617fc3716d73151031c5
Edivaldo Junior <112716389+edivaldodoj@users.noreply.github.com>
2024-08-27 02:22:06 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navLinks = document.querySelector('.nav-links');
+
+    menuToggle.addEventListener('click', function () {
+        menuToggle.classList.toggle('active');
+        navLinks.classList.toggle('active');
+    });
+});
