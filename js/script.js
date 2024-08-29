459581b9059a7e7a42a50e70cf357342c79912cd
Edivaldo Junior <112716389+edivaldodoj@users.noreply.github.com>
2024-08-28 22:30:38 -0300

    Delete js directory

--- js/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const menuToggle = document.querySelector('.menu-toggle');
-    const navLinks = document.querySelector('.nav-links');
-
-    menuToggle.addEventListener('click', function () {
-        menuToggle.classList.toggle('active');
-        navLinks.classList.toggle('active');
-    });
-});
