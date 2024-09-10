bc392362816a27800d71069f319551374161e984
Edivaldo Junior <112716389+edivaldodoj@users.noreply.github.com>
2024-09-09 22:28:05 -0300

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,31 @@
 // Script para o menu hamburger
+// document.addEventListener('DOMContentLoaded', function() {
+//     const menuToggle = document.querySelector('.menu-toggle');
+//     const navLinks = document.querySelector('.nav-links');
+
+//     menuToggle.addEventListener('click', function() {
+//         navLinks.classList.toggle('active');
+//         menuToggle.classList.toggle('active');
+//     });
+// });
+
+
 document.addEventListener('DOMContentLoaded', function() {
     const menuToggle = document.querySelector('.menu-toggle');
     const navLinks = document.querySelector('.nav-links');
+    const links = document.querySelectorAll('.nav-links a'); // Seleciona todos os links dentro do menu
 
+    // Alternar o menu quando o botão do hamburger for clicado
     menuToggle.addEventListener('click', function() {
         navLinks.classList.toggle('active');
         menuToggle.classList.toggle('active');
+    });
+
+    // Fecha o menu ao clicar em um dos links
+    links.forEach(function(link) {
+        link.addEventListener('click', function() {
+            navLinks.classList.remove('active');
+            menuToggle.classList.remove('active');
+        });
     });
 });
